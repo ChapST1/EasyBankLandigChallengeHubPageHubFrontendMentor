2e19dcc8465e3560e520834fb8c6f5d0f4367d6b
ChapST1 <1368422@senati.pe>
2022-02-07 18:41:12 -0500

    ll

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector("#btn");
+menu.addEventListener('click',()=>{
+    let barra1 = document.querySelector(".span1");
+    let barra2 = document.querySelector(".span2");
+    let barra3 = document.querySelector(".span3");
+    let capa = document.querySelector(".seccionUno");
+    barra1.classList.toggle("span1Active");
+    barra2.classList.toggle("span2Active");
+    barra3.classList.toggle("span3Active");
+    capa.classList.toggle("seccionUnoActive")
+})
